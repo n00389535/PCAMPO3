7f04ada2af8cf303bd3fef89dd1c48f4329105aa
Dandev <drodriguez@ulasalle.edu.pe>
2025-12-05 02:35:06 -0500

    Problema 1

--- PCAMPO3/Form1.Designer.cs
+++ PCAMPO3/Form1.Designer.cs
@@ -28,13 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Column0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnPromedioCurso = new System.Windows.Forms.Button();
+            this.btnEstudiante = new System.Windows.Forms.Button();
+            this.btnCurso = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(612, 88);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(123, 23);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Ingresar Aleatorio";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvDatos.Location = new System.Drawing.Point(22, 48);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(555, 305);
+            this.dgvDatos.TabIndex = 1;
+            // 
+            // Column0
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Column0.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Column0.HeaderText = "Fundamentos de Algoritmos";
+            this.Column0.Name = "Column0";
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Column1.HeaderText = "Base de Datos";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Column2.HeaderText = "Ingles";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Column3.HeaderText = "Prom, Estudia / Mejr Curso";
+            this.Column3.Name = "Column3";
+            // 
+            // btnPromedioCurso
+            // 
+            this.btnPromedioCurso.Location = new System.Drawing.Point(612, 148);
+            this.btnPromedioCurso.Name = "btnPromedioCurso";
+            this.btnPromedioCurso.Size = new System.Drawing.Size(123, 23);
+            this.btnPromedioCurso.TabIndex = 2;
+            this.btnPromedioCurso.Text = "Promedio Curso";
+            this.btnPromedioCurso.UseVisualStyleBackColor = true;
+            this.btnPromedioCurso.Click += new System.EventHandler(this.btnPromedioCurso_Click);
+            // 
+            // btnEstudiante
+            // 
+            this.btnEstudiante.Location = new System.Drawing.Point(612, 204);
+            this.btnEstudiante.Name = "btnEstudiante";
+            this.btnEstudiante.Size = new System.Drawing.Size(123, 23);
+            this.btnEstudiante.TabIndex = 3;
+            this.btnEstudiante.Text = "Promedio Estudiante";
+            this.btnEstudiante.UseVisualStyleBackColor = true;
+            this.btnEstudiante.Click += new System.EventHandler(this.btnEstudiante_Click);
+            // 
+            // btnCurso
+            // 
+            this.btnCurso.Location = new System.Drawing.Point(612, 261);
+            this.btnCurso.Name = "btnCurso";
+            this.btnCurso.Size = new System.Drawing.Size(123, 23);
+            this.btnCurso.TabIndex = 4;
+            this.btnCurso.Text = "Mejor Curso";
+            this.btnCurso.UseVisualStyleBackColor = true;
+            this.btnCurso.Click += new System.EventHandler(this.btnCurso_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCurso);
+            this.Controls.Add(this.btnEstudiante);
+            this.Controls.Add(this.btnPromedioCurso);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnRandom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnPromedioCurso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnEstudiante;
+        private System.Windows.Forms.Button btnCurso;
     }
 }
 
--- PCAMPO3/Form1.cs
+++ PCAMPO3/Form1.cs
@@ -12,9 +12,104 @@ namespace PCAMPO3
 {
     public partial class Form1 : Form
     {
+        double[,] notas = new double[11, 4];
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+
+            dgvDatos.Rows.Clear();
+
+            for (int k = 0; k < 11; k++)
+            {
+                dgvDatos.Rows.Add();
+                if(k == 10)
+                {
+                    dgvDatos.Rows[k].HeaderCell.Value = ("Prom. Curso");
+                }
+                else
+                {
+                    dgvDatos.Rows[k].HeaderCell.Value = ("Estudiante " + (k + 1));
+                }
+            }
+            dgvDatos.RowHeadersWidth = 150;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    notas[i, j] = rdn.Next(0, 20);
+                    dgvDatos.Rows[i].Cells[j].Value = notas[i, j];
+                }
+            }
+        }
+
+        private void btnPromedioCurso_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                double suma = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    suma += notas[i, j];
+                }
+                double promedio = (double)suma / 10;
+                notas[10, j] = promedio;
+                dgvDatos.Rows[10].Cells[j].Value = notas[10, j];
+            }
+        }
+
+        private void btnEstudiante_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                double suma = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    suma += notas[i, j];
+                }
+                double promedio = Math.Round(suma / 3, 2);
+                notas[i, 3] = promedio;
+                dgvDatos.Rows[i].Cells[3].Value = notas[i, 3];
+            }
+        }
+
+        private void btnCurso_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                double mejor = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if(notas[i, j] > mejor)
+                    {
+                        mejor = notas[i, j];
+                    }
+                }
+                dgvDatos.Rows[i].Cells[3].Value = mejor;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
     }
 }
